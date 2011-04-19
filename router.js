8d587286f32c2341e5cb3f9559c16d645a6ca803
Michael Warkentin <mwarkentin@gmail.com>
2011-04-19 15:16:11 -0400

    initial code for node beginner book

---
+++ router.js
@@ -0,0 +1,5 @@
+function route(resource) {
+    console.log('About to route a request for ' + resource);
+}
+
+exports.route = route;
---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var url = require('url');
+
+function start(route) {
+    function handle(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log('Request for ' + pathname + ' received.');
+        
+        route(pathname);
+        
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write('Hello world.');
+        response.end();
+    }
+    
+    http.createServer(handle).listen(8888);
+    console.log('Server has started.');
+}
+
+exports.start = start;
